596b8ba990845d9eb32161250d6bca8d68b65e3d
Markus Reiter <me@reitermark.us>
2020-07-05 03:35:08 +0200

    Remove unsupported atomic instructions.

--- app/components/compiler_builtins/atomics.c
+++ app/components/compiler_builtins/atomics.c
@@ -3,422 +3,182 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-int8_t sync_fetch_and_add_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_fetch_and_add_1(int8_t *ptr, int8_t arg) {
   return __sync_fetch_and_add_1(ptr, arg);
 }
 
-int8_t __sync_fetch_and_add_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_fetch_and_add_1")));
-
-int8_t sync_fetch_and_sub_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_fetch_and_sub_1(int8_t *ptr, int8_t arg) {
   return __sync_fetch_and_sub_1(ptr, arg);
 }
 
-int8_t __sync_fetch_and_sub_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_fetch_and_sub_1")));
-
-int8_t sync_fetch_and_or_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_fetch_and_or_1(int8_t *ptr, int8_t arg) {
   return __sync_fetch_and_or_1(ptr, arg);
 }
 
-int8_t __sync_fetch_and_or_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_fetch_and_or_1")));
-
-int8_t sync_fetch_and_and_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_fetch_and_and_1(int8_t *ptr, int8_t arg) {
   return __sync_fetch_and_and_1(ptr, arg);
 }
 
-int8_t __sync_fetch_and_and_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_fetch_and_and_1")));
-
-int8_t sync_fetch_and_xor_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_fetch_and_xor_1(int8_t *ptr, int8_t arg) {
   return __sync_fetch_and_xor_1(ptr, arg);
 }
 
-int8_t __sync_fetch_and_xor_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_fetch_and_xor_1")));
-
-int8_t sync_fetch_and_nand_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_fetch_and_nand_1(int8_t *ptr, int8_t arg) {
   return __sync_fetch_and_nand_1(ptr, arg);
 }
 
-int8_t __sync_fetch_and_nand_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_fetch_and_nand_1")));
-
-int8_t sync_add_and_fetch_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_add_and_fetch_1(int8_t *ptr, int8_t arg) {
   return __sync_add_and_fetch_1(ptr, arg);
 }
 
-int8_t __sync_add_and_fetch_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_add_and_fetch_1")));
-
-int8_t sync_sub_and_fetch_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_sub_and_fetch_1(int8_t *ptr, int8_t arg) {
   return __sync_sub_and_fetch_1(ptr, arg);
 }
 
-int8_t __sync_sub_and_fetch_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_sub_and_fetch_1")));
-
-int8_t sync_or_and_fetch_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_or_and_fetch_1(int8_t *ptr, int8_t arg) {
   return __sync_or_and_fetch_1(ptr, arg);
 }
 
-int8_t __sync_or_and_fetch_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_or_and_fetch_1")));
-
-int8_t sync_and_and_fetch_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_and_and_fetch_1(int8_t *ptr, int8_t arg) {
   return __sync_and_and_fetch_1(ptr, arg);
 }
 
-int8_t __sync_and_and_fetch_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_and_and_fetch_1")));
-
-int8_t sync_xor_and_fetch_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_xor_and_fetch_1(int8_t *ptr, int8_t arg) {
   return __sync_xor_and_fetch_1(ptr, arg);
 }
 
-int8_t __sync_xor_and_fetch_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_xor_and_fetch_1")));
-
-int8_t sync_nand_and_fetch_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_nand_and_fetch_1(int8_t *ptr, int8_t arg) {
   return __sync_nand_and_fetch_1(ptr, arg);
 }
 
-int8_t __sync_nand_and_fetch_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_nand_and_fetch_1")));
-
-int8_t sync_lock_test_and_set_1(int8_t *ptr, int8_t arg) {
+int8_t __sync_lock_test_and_set_1(int8_t *ptr, int8_t arg) {
   return __sync_lock_test_and_set_1(ptr, arg);
 }
 
-int8_t __sync_lock_test_and_set_1(int8_t *ptr, int8_t arg)
-  __attribute__((weak,alias("sync_lock_test_and_set_1")));
-
-int8_t sync_val_compare_and_swap_1(int8_t *ptr, int8_t old, int8_t new) {
+int8_t __sync_val_compare_and_swap_1(int8_t *ptr, int8_t old, int8_t new) {
   return __sync_val_compare_and_swap_1(ptr, old, new);
 }
 
-int8_t __sync_val_compare_and_swap_1(int8_t *ptr, int8_t old, int8_t new)
-  __attribute__((weak,alias("sync_val_compare_and_swap_1")));
-
-bool sync_bool_compare_and_swap_1(int8_t *ptr, int8_t old, int8_t new) {
+bool __sync_bool_compare_and_swap_1(int8_t *ptr, int8_t old, int8_t new) {
   return __sync_bool_compare_and_swap_1(ptr, old, new);
 }
 
-bool __sync_bool_compare_and_swap_1(int8_t *ptr, int8_t old, int8_t new)
-  __attribute__((weak,alias("sync_bool_compare_and_swap_1")));
-
-int16_t sync_fetch_and_add_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_fetch_and_add_2(int16_t *ptr, int16_t arg) {
   return __sync_fetch_and_add_2(ptr, arg);
 }
 
-int16_t __sync_fetch_and_add_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_fetch_and_add_2")));
-
-int16_t sync_fetch_and_sub_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_fetch_and_sub_2(int16_t *ptr, int16_t arg) {
   return __sync_fetch_and_sub_2(ptr, arg);
 }
 
-int16_t __sync_fetch_and_sub_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_fetch_and_sub_2")));
-
-int16_t sync_fetch_and_or_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_fetch_and_or_2(int16_t *ptr, int16_t arg) {
   return __sync_fetch_and_or_2(ptr, arg);
 }
 
-int16_t __sync_fetch_and_or_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_fetch_and_or_2")));
-
-int16_t sync_fetch_and_and_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_fetch_and_and_2(int16_t *ptr, int16_t arg) {
   return __sync_fetch_and_and_2(ptr, arg);
 }
 
-int16_t __sync_fetch_and_and_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_fetch_and_and_2")));
-
-int16_t sync_fetch_and_xor_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_fetch_and_xor_2(int16_t *ptr, int16_t arg) {
   return __sync_fetch_and_xor_2(ptr, arg);
 }
 
-int16_t __sync_fetch_and_xor_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_fetch_and_xor_2")));
-
-int16_t sync_fetch_and_nand_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_fetch_and_nand_2(int16_t *ptr, int16_t arg) {
   return __sync_fetch_and_nand_2(ptr, arg);
 }
 
-int16_t __sync_fetch_and_nand_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_fetch_and_nand_2")));
-
-int16_t sync_add_and_fetch_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_add_and_fetch_2(int16_t *ptr, int16_t arg) {
   return __sync_add_and_fetch_2(ptr, arg);
 }
 
-int16_t __sync_add_and_fetch_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_add_and_fetch_2")));
-
-int16_t sync_sub_and_fetch_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_sub_and_fetch_2(int16_t *ptr, int16_t arg) {
   return __sync_sub_and_fetch_2(ptr, arg);
 }
 
-int16_t __sync_sub_and_fetch_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_sub_and_fetch_2")));
-
-int16_t sync_or_and_fetch_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_or_and_fetch_2(int16_t *ptr, int16_t arg) {
   return __sync_or_and_fetch_2(ptr, arg);
 }
 
-int16_t __sync_or_and_fetch_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_or_and_fetch_2")));
-
-int16_t sync_and_and_fetch_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_and_and_fetch_2(int16_t *ptr, int16_t arg) {
   return __sync_and_and_fetch_2(ptr, arg);
 }
 
-int16_t __sync_and_and_fetch_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_and_and_fetch_2")));
-
-int16_t sync_xor_and_fetch_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_xor_and_fetch_2(int16_t *ptr, int16_t arg) {
   return __sync_xor_and_fetch_2(ptr, arg);
 }
 
-int16_t __sync_xor_and_fetch_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_xor_and_fetch_2")));
-
-int16_t sync_nand_and_fetch_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_nand_and_fetch_2(int16_t *ptr, int16_t arg) {
   return __sync_nand_and_fetch_2(ptr, arg);
 }
 
-int16_t __sync_nand_and_fetch_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_nand_and_fetch_2")));
-
-int16_t sync_lock_test_and_set_2(int16_t *ptr, int16_t arg) {
+int16_t __sync_lock_test_and_set_2(int16_t *ptr, int16_t arg) {
   return __sync_lock_test_and_set_2(ptr, arg);
 }
 
-int16_t __sync_lock_test_and_set_2(int16_t *ptr, int16_t arg)
-  __attribute__((weak,alias("sync_lock_test_and_set_2")));
-
-int16_t sync_val_compare_and_swap_2(int16_t *ptr, int16_t old, int16_t new) {
+int16_t __sync_val_compare_and_swap_2(int16_t *ptr, int16_t old, int16_t new) {
   return __sync_val_compare_and_swap_2(ptr, old, new);
 }
 
-int16_t __sync_val_compare_and_swap_2(int16_t *ptr, int16_t old, int16_t new)
-  __attribute__((weak,alias("sync_val_compare_and_swap_2")));
-
-bool sync_bool_compare_and_swap_2(int16_t *ptr, int16_t old, int16_t new) {
+bool __sync_bool_compare_and_swap_2(int16_t *ptr, int16_t old, int16_t new) {
   return __sync_bool_compare_and_swap_2(ptr, old, new);
 }
 
-bool __sync_bool_compare_and_swap_2(int16_t *ptr, int16_t old, int16_t new)
-  __attribute__((weak,alias("sync_bool_compare_and_swap_2")));
-
-int32_t sync_fetch_and_add_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_fetch_and_add_4(int32_t *ptr, int32_t arg) {
   return __sync_fetch_and_add_4(ptr, arg);
 }
 
-int32_t __sync_fetch_and_add_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_fetch_and_add_4")));
-
-int32_t sync_fetch_and_sub_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_fetch_and_sub_4(int32_t *ptr, int32_t arg) {
   return __sync_fetch_and_sub_4(ptr, arg);
 }
 
-int32_t __sync_fetch_and_sub_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_fetch_and_sub_4")));
-
-int32_t sync_fetch_and_or_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_fetch_and_or_4(int32_t *ptr, int32_t arg) {
   return __sync_fetch_and_or_4(ptr, arg);
 }
 
-int32_t __sync_fetch_and_or_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_fetch_and_or_4")));
-
-int32_t sync_fetch_and_and_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_fetch_and_and_4(int32_t *ptr, int32_t arg) {
   return __sync_fetch_and_and_4(ptr, arg);
 }
 
-int32_t __sync_fetch_and_and_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_fetch_and_and_4")));
-
-int32_t sync_fetch_and_xor_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_fetch_and_xor_4(int32_t *ptr, int32_t arg) {
   return __sync_fetch_and_xor_4(ptr, arg);
 }
 
-int32_t __sync_fetch_and_xor_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_fetch_and_xor_4")));
-
-int32_t sync_fetch_and_nand_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_fetch_and_nand_4(int32_t *ptr, int32_t arg) {
   return __sync_fetch_and_nand_4(ptr, arg);
 }
 
-int32_t __sync_fetch_and_nand_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_fetch_and_nand_4")));
-
-int32_t sync_add_and_fetch_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_add_and_fetch_4(int32_t *ptr, int32_t arg) {
   return __sync_add_and_fetch_4(ptr, arg);
 }
 
-int32_t __sync_add_and_fetch_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_add_and_fetch_4")));
-
-int32_t sync_sub_and_fetch_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_sub_and_fetch_4(int32_t *ptr, int32_t arg) {
   return __sync_sub_and_fetch_4(ptr, arg);
 }
 
-int32_t __sync_sub_and_fetch_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_sub_and_fetch_4")));
-
-int32_t sync_or_and_fetch_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_or_and_fetch_4(int32_t *ptr, int32_t arg) {
   return __sync_or_and_fetch_4(ptr, arg);
 }
 
-int32_t __sync_or_and_fetch_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_or_and_fetch_4")));
-
-int32_t sync_and_and_fetch_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_and_and_fetch_4(int32_t *ptr, int32_t arg) {
   return __sync_and_and_fetch_4(ptr, arg);
 }
 
-int32_t __sync_and_and_fetch_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_and_and_fetch_4")));
-
-int32_t sync_xor_and_fetch_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_xor_and_fetch_4(int32_t *ptr, int32_t arg) {
   return __sync_xor_and_fetch_4(ptr, arg);
 }
 
-int32_t __sync_xor_and_fetch_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_xor_and_fetch_4")));
-
-int32_t sync_nand_and_fetch_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_nand_and_fetch_4(int32_t *ptr, int32_t arg) {
   return __sync_nand_and_fetch_4(ptr, arg);
 }
 
-int32_t __sync_nand_and_fetch_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_nand_and_fetch_4")));
-
-int32_t sync_lock_test_and_set_4(int32_t *ptr, int32_t arg) {
+int32_t __sync_lock_test_and_set_4(int32_t *ptr, int32_t arg) {
   return __sync_lock_test_and_set_4(ptr, arg);
 }
 
-int32_t __sync_lock_test_and_set_4(int32_t *ptr, int32_t arg)
-  __attribute__((weak,alias("sync_lock_test_and_set_4")));
-
-int32_t sync_val_compare_and_swap_4(int32_t *ptr, int32_t old, int32_t new) {
+int32_t __sync_val_compare_and_swap_4(int32_t *ptr, int32_t old, int32_t new) {
   return __sync_val_compare_and_swap_4(ptr, old, new);
 }
 
-int32_t __sync_val_compare_and_swap_4(int32_t *ptr, int32_t old, int32_t new)
-  __attribute__((weak,alias("sync_val_compare_and_swap_4")));
-
-bool sync_bool_compare_and_swap_4(int32_t *ptr, int32_t old, int32_t new) {
+bool __sync_bool_compare_and_swap_4(int32_t *ptr, int32_t old, int32_t new) {
   return __sync_bool_compare_and_swap_4(ptr, old, new);
 }
-
-bool __sync_bool_compare_and_swap_4(int32_t *ptr, int32_t old, int32_t new)
-  __attribute__((weak,alias("sync_bool_compare_and_swap_4")));
-
-int64_t sync_fetch_and_add_8(int64_t *ptr, int64_t arg) {
-  return __sync_fetch_and_add_8(ptr, arg);
-}
-
-int64_t __sync_fetch_and_add_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_fetch_and_add_8")));
-
-int64_t sync_fetch_and_sub_8(int64_t *ptr, int64_t arg) {
-  return __sync_fetch_and_sub_8(ptr, arg);
-}
-
-int64_t __sync_fetch_and_sub_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_fetch_and_sub_8")));
-
-int64_t sync_fetch_and_or_8(int64_t *ptr, int64_t arg) {
-  return __sync_fetch_and_or_8(ptr, arg);
-}
-
-int64_t __sync_fetch_and_or_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_fetch_and_or_8")));
-
-int64_t sync_fetch_and_and_8(int64_t *ptr, int64_t arg) {
-  return __sync_fetch_and_and_8(ptr, arg);
-}
-
-int64_t __sync_fetch_and_and_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_fetch_and_and_8")));
-
-int64_t sync_fetch_and_xor_8(int64_t *ptr, int64_t arg) {
-  return __sync_fetch_and_xor_8(ptr, arg);
-}
-
-int64_t __sync_fetch_and_xor_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_fetch_and_xor_8")));
-
-int64_t sync_fetch_and_nand_8(int64_t *ptr, int64_t arg) {
-  return __sync_fetch_and_nand_8(ptr, arg);
-}
-
-int64_t __sync_fetch_and_nand_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_fetch_and_nand_8")));
-
-int64_t sync_add_and_fetch_8(int64_t *ptr, int64_t arg) {
-  return __sync_add_and_fetch_8(ptr, arg);
-}
-
-int64_t __sync_add_and_fetch_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_add_and_fetch_8")));
-
-int64_t sync_sub_and_fetch_8(int64_t *ptr, int64_t arg) {
-  return __sync_sub_and_fetch_8(ptr, arg);
-}
-
-int64_t __sync_sub_and_fetch_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_sub_and_fetch_8")));
-
-int64_t sync_or_and_fetch_8(int64_t *ptr, int64_t arg) {
-  return __sync_or_and_fetch_8(ptr, arg);
-}
-
-int64_t __sync_or_and_fetch_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_or_and_fetch_8")));
-
-int64_t sync_and_and_fetch_8(int64_t *ptr, int64_t arg) {
-  return __sync_and_and_fetch_8(ptr, arg);
-}
-
-int64_t __sync_and_and_fetch_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_and_and_fetch_8")));
-
-int64_t sync_xor_and_fetch_8(int64_t *ptr, int64_t arg) {
-  return __sync_xor_and_fetch_8(ptr, arg);
-}
-
-int64_t __sync_xor_and_fetch_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_xor_and_fetch_8")));
-
-int64_t sync_nand_and_fetch_8(int64_t *ptr, int64_t arg) {
-  return __sync_nand_and_fetch_8(ptr, arg);
-}
-
-int64_t __sync_nand_and_fetch_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_nand_and_fetch_8")));
-
-int64_t sync_lock_test_and_set_8(int64_t *ptr, int64_t arg) {
-  return __sync_lock_test_and_set_8(ptr, arg);
-}
-
-int64_t __sync_lock_test_and_set_8(int64_t *ptr, int64_t arg)
-  __attribute__((weak,alias("sync_lock_test_and_set_8")));
-
-int64_t sync_val_compare_and_swap_8(int64_t *ptr, int64_t old, int64_t new) {
-  return __sync_val_compare_and_swap_8(ptr, old, new);
-}
-
-int64_t __sync_val_compare_and_swap_8(int64_t *ptr, int64_t old, int64_t new)
-  __attribute__((weak,alias("sync_val_compare_and_swap_8")));
-
-bool sync_bool_compare_and_swap_8(int64_t *ptr, int64_t old, int64_t new) {
-  return __sync_bool_compare_and_swap_8(ptr, old, new);
-}
-
-bool __sync_bool_compare_and_swap_8(int64_t *ptr, int64_t old, int64_t new)
-  __attribute__((weak,alias("sync_bool_compare_and_swap_8")));
